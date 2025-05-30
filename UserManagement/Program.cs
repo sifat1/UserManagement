2400b21ea826d0b88acbe33e8001888ac6537de0
sifat1 <sifatshikdhar@gmail.com>
2025-05-30 07:31:10 +0600

    mod ps

--- UserManagement/Program.cs
+++ UserManagement/Program.cs
@@ -42,7 +42,7 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    var db = scope.ServiceProvider.GetRequiredService<DBContext>();
     db.Database.Migrate();
 }
 
